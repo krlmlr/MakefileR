2ba8375d3433087aeb84c788cbe90ad1b2750e9f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-17 22:48:34 +0200

    more tests for dep-less rule

--- tests/testthat/test-rule.R
+++ tests/testthat/test-rule.R
@@ -2,4 +2,6 @@ context("rule")
 
 test_that("target-only rule", {
   expect_equal(format(create_make_rule(".FORCE")), ".FORCE:")
+  expect_equal(format(create_make_rule("target", script = "false")),
+               c("target:", "\tfalse"))
 })
