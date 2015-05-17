9dbde2c03c45392931c20ede294c8e5a5935db03
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-17 23:14:16 +0200

    don't edit

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -2,6 +2,6 @@ context("file")
 
 test_that("Empty Makefile includes only comment", {
   makefile <- format(create_makefile())
-  expect_match(makefile, "^# .*$")
+  expect_match(makefile, "^# .* do not edit.*$")
   expect_equal(length(makefile), 1)
 })
