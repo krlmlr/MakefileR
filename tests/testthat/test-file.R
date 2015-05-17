4d356579d559616a41c35716913be91070c14d5c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-17 23:17:43 +0200

    test printing

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -5,3 +5,11 @@ test_that("Empty Makefile includes only comment", {
   expect_match(makefile, "^# .* do not edit.*$")
   expect_equal(length(makefile), 1)
 })
+
+test_that("Printing works as expected", {
+  with_mock(
+    cat = identity,
+    makefile <- print(create_makefile()))
+  expect_match(makefile, "^# .* do not edit.*\n$")
+  expect_equal(length(makefile), 1)
+})
