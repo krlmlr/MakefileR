5038c3c70461e069c1e9bc9dd166ff71d000127f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-17 22:50:31 +0200

    test for target-dep rules

--- tests/testthat/test-rule.R
+++ tests/testthat/test-rule.R
@@ -5,3 +5,9 @@ test_that("target-only rule", {
   expect_equal(format(create_make_rule("target", script = "false")),
                c("target:", "\tfalse"))
 })
+
+test_that("target-dep rule", {
+  expect_equal(format(create_make_rule("a", c("b", "c"))), "a: b c")
+  expect_equal(format(create_make_rule("a", "b", c("true", "false"))),
+               c("a: b", "\ttrue", "\tfalse"))
+})
