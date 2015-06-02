7d8bbf31ec69bb71752cc98f91e82fdee1751bf4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-02 10:18:49 +0200

    adapt tests

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -1,7 +1,7 @@
 context("file")
 
 test_that("Empty Makefile includes only comment", {
-  makefile <- format(create_makefile())
+  makefile <- format(makefile())
   expect_match(makefile, "^# .* do not edit.*$")
   expect_equal(length(makefile), 1)
 })
@@ -9,7 +9,7 @@ test_that("Empty Makefile includes only comment", {
 test_that("Printing works as expected", {
   with_mock(
     cat = function(x, sep) x,
-    makefile <- print(create_makefile()))
+    makefile <- print(makefile()))
   expect_match(makefile, "^# .* do not edit.*\n$")
   expect_equal(length(makefile), 1)
 })
